06b6ad5d2ec1c7d8929d053f7d22e0797647327d
TC <you@example.com>
2022-11-07 11:36:37 +0100

    oblicznaie miejsc zerowych ax+by+c=0 ver 1.4

---
+++ axbyc.cpp
@@ -0,0 +1,10 @@
+#include "prehead.h"
+
+float func_axbyc(float a, float c)
+/* 
+    zgodnie z przeksztalceniami wzoru Ax + By + C = 0 gdzie B != 0,
+    to otrzymujemy wzor na miejsce zerowe = C / A
+*/
+{
+    return c/a;
+}
